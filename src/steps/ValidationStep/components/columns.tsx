f2f04a01a9e57d4270e399d22e7034009dbfc188
Rlopeze <mumolopeze@uc.cl>
2024-02-28 11:42:20 -0300

    fix(fix): :ambulance: uncomment editable and formatter

--- src/steps/ValidationStep/components/columns.tsx
+++ src/steps/ValidationStep/components/columns.tsx
@@ -64,7 +64,7 @@ export const generateColumns = <T extends string>(fields: Fields<T>): Column<Dat
           )}
         </Box>
       ),
-      // editable: column.fieldType.type !== "checkbox",
+      editable: true,
       editor: ({ row, onRowChange, onClose }) => {
         let component
 
@@ -103,54 +103,54 @@ export const generateColumns = <T extends string>(fields: Fields<T>): Column<Dat
       editorOptions: {
         editOnClick: true,
       },
-      // formatter: ({ row, onRowChange }) => {
-      //   let component
+      formatter: ({ row, onRowChange }) => {
+        let component
 
-      //   switch (column.fieldType.type) {
-      //     case "checkbox":
-      //       component = (
-      //         <Box
-      //           display="flex"
-      //           alignItems="center"
-      //           height="100%"
-      //           onClick={(event) => {
-      //             event.stopPropagation()
-      //           }}
-      //         >
-      //           <Switch
-      //             isChecked={row[column.key] as boolean}
-      //             onChange={() => {
-      //               onRowChange({ ...row, [column.key]: !row[column.key as T] })
-      //             }}
-      //           />
-      //         </Box>
-      //       )
-      //       break
-      //     case "select":
-      //       component = (
-      //         <Box minWidth="100%" minHeight="100%" overflow="hidden" textOverflow="ellipsis">
-      //           {column.fieldType.options.find((option) => option.value === row[column.key as T])?.label || null}
-      //         </Box>
-      //       )
-      //       break
-      //     default:
-      //       component = (
-      //         <Box minWidth="100%" minHeight="100%" overflow="hidden" textOverflow="ellipsis">
-      //           {row[column.key as T]}
-      //         </Box>
-      //       )
-      //   }
+        switch (column.fieldType.type) {
+          case "checkbox":
+            component = (
+              <Box
+                display="flex"
+                alignItems="center"
+                height="100%"
+                onClick={(event) => {
+                  event.stopPropagation()
+                }}
+              >
+                <Switch
+                  isChecked={row[column.key] as boolean}
+                  onChange={() => {
+                    onRowChange({ ...row, [column.key]: !row[column.key as T] })
+                  }}
+                />
+              </Box>
+            )
+            break
+          case "select":
+            component = (
+              <Box minWidth="100%" minHeight="100%" overflow="hidden" textOverflow="ellipsis">
+                {column.fieldType.options.find((option) => option.value === row[column.key as T])?.label || null}
+              </Box>
+            )
+            break
+          default:
+            component = (
+              <Box minWidth="100%" minHeight="100%" overflow="hidden" textOverflow="ellipsis">
+                {row[column.key as T]}
+              </Box>
+            )
+        }
 
-      //   if (row.__errors?.[column.key]) {
-      //     return (
-      //       <Tooltip placement="top" hasArrow label={row.__errors?.[column.key]?.message}>
-      //         {component}
-      //       </Tooltip>
-      //     )
-      //   }
+        if (row.__errors?.[column.key]) {
+          return (
+            <Tooltip placement="top" hasArrow label={row.__errors?.[column.key]?.message}>
+              {component}
+            </Tooltip>
+          )
+        }
 
-      //   return component
-      // },
+        return component
+      },
       cellClass: (row: Meta) => {
         switch (row.__errors?.[column.key]?.level) {
           case "error":
